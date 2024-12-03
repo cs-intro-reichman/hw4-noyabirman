3ba94ecc8aaab4d7f6d8c937944d955a1c73bb19
Noya Birman <noya.birman@post.runi.ac.il>
2024-12-03 15:05:14 +0200

    completed

--- MyString.java
+++ MyString.java
@@ -18,13 +18,45 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        if (str == null) {
+            return null ;
+        }
+        if (str.length() == 0) {
+            return "";
+        }
+
+        String result="" ;
+        for (int i=0;i<str.length() ; i++) {
+            char ch=str.charAt(i);
+            if (ch>='A' && ch<= 'Z'){
+                ch=(char)(ch+32);
+            }
+            result+=ch;
+        } 
+        return result;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str1==null||str2==null) {
+        return false;
+        }
+
+        if (str1.length()<str2.length()){
+            return false;
+        }
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            boolean match=true;
+            for (int j=0 ; i<str2.length() ; j++){
+                if (str1.charAt(i+j) != str2.charAt(j)) {
+                    match=false;
+                    break;
+                }
+            }
+            if (match) {
+                return true;
+            }
+        }
         return false;
     }
 }
